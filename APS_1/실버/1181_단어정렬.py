d9235da567a7fee5185c30a43895d77aac1a1e7b
kimilho <kih7495@naver.com>
2024-03-05 09:12:56 +0900

    update

--- APS_1/실버/1181_단어정렬.py
+++ APS_1/실버/1181_단어정렬.py
@@ -9,8 +9,12 @@ for i in range(N):
     s = input()
     my_set.add(s)
     my_list = list(my_set)
+my_list.sort()
+my_list.sort(key=len)
+# print(my_list)
+# 알파벳을 그냥 정렬하면 abc순으로 된다..
+# 글자수대로 정렬하고 싶으면 key=len을 사용하면 된다. 근데 글자수대로 정렬되고 사전순으로는 되지 않는다. 순서가 바뀜
 # print(my_list)
 for i in range(len(my_list)):
-    lst.append(len(my_list[i]))
-    lst.sort()
-print(lst)
+    print(my_list[i])
+
